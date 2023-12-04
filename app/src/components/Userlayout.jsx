7711fa1101de78e8e14489fe29e6d321826453e8
raj720s <neutronstarkd@gmail.com>
2023-12-04 15:19:27 +0530

    removed some code

--- app/src/components/Userlayout.jsx
+++ app/src/components/Userlayout.jsx
@@ -5,35 +5,10 @@ import { Navigate, Outlet, useNavigate } from 'react-router-dom'
 import '../styles/index.scss'
 import { ToastContainer } from 'react-toastify'
 import UserHeader from './header/UserHeader'
-import { useAuth0 } from '@auth0/auth0-react'
 function Userlayout(props) {
   // if (!props.user) {
   //   <Navigate to={'/Login'} />
   // }
-  const navigate = useNavigate()
-
-  const { user, loginWithRedirect, isAuthenticated } = useAuth0()
-
-  console.log({
-    user, isAuthenticated
-  })
-  useEffect(() => {
-    if (isAuthenticated) {
-      // psw-less login 
-      const data = {
-        email: user.email,
-      }
-      axiosIntance.post('/auth/user-login', data).then(res => {
-        console.log(res.data)
-        localStorage.setItem('token', res.data.token)
-        return navigate('/user/dashboard')
-      }).catch(e => {
-        toast.error(e.message || 'login failed', {
-          position: toast.POSITION.TOP_RIGHT,
-        });
-      })
-    }
-  }, [isAuthenticated])
 
   return (
     <div className='layout-default'>
